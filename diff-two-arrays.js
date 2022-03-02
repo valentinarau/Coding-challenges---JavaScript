9a1076b41dc845f2f3f2a17082ad8443122b0341
valentinarau <82102044+valentinarau@users.noreply.github.com>
2022-03-01 21:36:23 -0300

    Update diff-two-arrays.js

--- diff-two-arrays.js
+++ diff-two-arrays.js
@@ -2,7 +2,19 @@
 
 function diffArray(arr1, arr2) {
   const newArr = 
-  arr1.concat(arr2).filter(a => !(arr1.includes(a) && arr2.includes(a)))
+  arr1
+  .concat(arr2)
+  .filter(a => !(arr1.includes(a) && arr2.includes(a)))
 
   return newArr;
+}
+
+// Other FCC Fancy Solution
+
+function diffArray(arr1, arr2) {
+  return [...diff(arr1, arr2), ...diff(arr2, arr1)];
+
+  function diff(a, b) {
+    return a.filter(item => b.indexOf(item) === -1);
+  }
 }
